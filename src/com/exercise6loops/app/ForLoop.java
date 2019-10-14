3566895eac4b22d5117c11025cd3ff1adaa07261
AlexAvilaT <alex.avilat6@gmail.com>
2019-10-14 13:05:38 -0500

    For loop exercises added. continue break sentences input.

--- src/com/exercise6loops/app/ForLoop.java
+++ src/com/exercise6loops/app/ForLoop.java
@@ -1,9 +1,18 @@
 package com.exercise6loops.app;
 
-public class ForLoop {
+public class ForLoop 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		// constant declaration
+		//Variable declaration
+		//Objects construction
+		// Create a series of 1-100 1 by 1
+		for(int i=0;i<100;i++)
+		{
+			System.out.println(i);
+		}
 
 	}
 
